5aad12bc30d6c7b259d4e00a9ca0922afdb32a91
Oner Tekin <tekinoner37@gmail.com>
2022-05-10 21:50:57 +0300

    10.05.2022

---
+++ src/test/java/day02_driverMethodlari/C05_getWndowHandle.java
@@ -0,0 +1,16 @@
+package day02_driverMethodlari;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C05_getWndowHandle {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        System.out.println(driver.getWindowHandle());
+        //CDwindow-658D6255BDB6149F8221D2F432864E23
+        //benzersiz olarak acilan windowa ait hashcode unu verir.
+    }
+}
